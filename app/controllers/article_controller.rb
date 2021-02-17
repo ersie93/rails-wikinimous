668b7c3d49d3b3adb423ee943153d396962c6bb1
ryan erswell <ersie93@googlemail.com>
2021-02-17 10:52:31 +0000

    unfinished

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -3,4 +3,24 @@ class ArticleController < ApplicationController
 	def index
 		@articles = Article.all
 	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(params_article)
+		@article.save
+		redirect_to article_path(@article)
+	end
+
+	private
+
+	def params_article
+		params.require(:article).permit(:title, :content)
+	end
 end
